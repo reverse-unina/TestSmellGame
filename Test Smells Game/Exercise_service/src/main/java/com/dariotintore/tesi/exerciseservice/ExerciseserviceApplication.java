fab597905ea050180b349511339b335f1b565484
Loris Zannini <97295609+LZannini@users.noreply.github.com>
2024-07-18 13:08:46 +0200

    Add files via upload

---
+++ Test Smells Game/Exercise_service/src/main/java/com/dariotintore/tesi/exerciseservice/ExerciseserviceApplication.java
@@ -0,0 +1,13 @@
+package com.dariotintore.tesi.exerciseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseserviceApplication.class, args);
+	}
+
+}
