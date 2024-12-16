7060d0551b6c0c4d3a3a51694033fba81f5359ca
Michele Perlotto <mick0974@outlook.it>
2024-12-16 16:18:04 +0100

    Feat: Complete Spring Gateway configuration and resolve CORS 403 errors
    This update finalizes the Spring Gateway configuration and resolves the CORS issue by:
    - Defining allowed URL patterns in the Spring Gateway to route requests correctly to the microservices.
    - Adding a CORS configuration bean to each microservice to explicitly allow requests from the frontend or other specified origins.
    
    This ensures that the Spring Gateway routes requests correctly to the backend services and that cross-origin requests are handled without issues.

--- Test-Smells-Game/Projects/Leaderboard_service/src/main/java/com/dariotintore/tesi/leaderboard/LeaderboardApplication.java
+++ Test-Smells-Game/Projects/Leaderboard_service/src/main/java/com/dariotintore/tesi/leaderboard/LeaderboardApplication.java
@@ -2,12 +2,26 @@ package com.dariotintore.tesi.leaderboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LeaderboardApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(LeaderboardApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:*", "https://*.trycloudflare.com", "https://*.ngrok-free.app");
+      }
+    };
   }
 
 }
