6455699db431b0fd31ab873165aef96d61279395
Michele Perlotto <mick0974@outlook.it>
2025-01-16 16:22:35 +0100

    Feat: Validate support for JUnit5
    The Compiler Service was already designed to be independent of the JUnit version used in refactoring exercises, allowing each exercise to specify a different JUnit version as a list of Maven dependencies in the configuration file.
    
    This update includes the following changes:
    - Change the handling of the dependencies listed in the config files, expressing them as list of strings (ideally, one for each dependecy), rather than a single string;
    -  Provide the refactoring exercise "Calculator" adapted for JUnit5 as the new exercise "Calculator_JUnit5".

--- Test-Smells-Game/Projects/Compiler_service/external/server/dependency-manger.js
+++ Test-Smells-Game/Projects/Compiler_service/external/server/dependency-manger.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 
 function editPom(exerciseConfiguration){
   let data = fs.readFileSync(process.env.ROOT_PATH + "external/compiler/tesi/pom_template.txt", 'utf8')
-  data = data.replace('{dependency}',exerciseConfiguration.dependencies)
+  let dependencies = "";
+  exerciseConfiguration.dependencies.forEach(dep => dependencies += dep);
+  data = data.replace('{dependency}', dependencies)
   fs.writeFileSync(process.env.ROOT_PATH + "external/compiler/tesi/pom.xml", data)
 }
 
